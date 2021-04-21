01440328e0cf0c7197aca9e2a88ec845a6df7542
Alexsandro Pires <alexsandropires@MacBook-Air-de-Alexsandro.local>
2021-04-21 19:31:39 -0300

    18. Criando operação de instanciação

--- src/main/java/br/com/alphapires/fullStack/FullStackApplication.java
+++ src/main/java/br/com/alphapires/fullStack/FullStackApplication.java
@@ -1,13 +1,29 @@
 package br.com.alphapires.fullStack;
 
+import br.com.alphapires.fullStack.domain.Categoria;
+import br.com.alphapires.fullStack.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class FullStackApplication {
+public class FullStackApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FullStackApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
